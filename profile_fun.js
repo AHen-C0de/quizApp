62ebb833f418fce5b4c9e22093269ead57e5652e
Angela Hentschel <angela.neuron@gmail.com>
2022-09-05 19:09:52 +0200

    add more responsive design

--- profile_fun.js
+++ profile_fun.js
@@ -4,8 +4,12 @@ console.log("Js is read!");
 const sliderDarkMode = document.querySelector('[js-data="dark-mode"]');
 const body = document.querySelector("body");
 const settingsBox = document.querySelector('[js-data="settingsBox"]');
+const borderDark = document.querySelector('[js-data="border-dark"]');
 
 sliderDarkMode.addEventListener("click", () => {
-  body.classList.toggle("darkModeBody");
-  settingsBox.classList.toggle("darkModeCard");
+  body.classList.toggle("darkMode__body");
+  settingsBox.classList.toggle("darkMode__card");
+  borderDark.classList.toggle("darkMode__border-dark");
+
+  console.log(borderDark);
 });
