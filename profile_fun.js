1610f196cd81ab561e8dc95d891f850d6d6a050e
Angela Hentschel <angela.neuron@gmail.com>
2022-09-06 11:27:31 +0200

    add dark-mode functionality in profile_fun.js

--- profile_fun.js
+++ profile_fun.js
@@ -1,15 +1,32 @@
 console.clear();
 console.log("Js is read!");
 
+let darkMode;
+
 const sliderDarkMode = document.querySelector('[js-data="dark-mode"]');
 const body = document.querySelector("body");
 const settingsBox = document.querySelector('[js-data="settingsBox"]');
-const borderDark = document.querySelector('[js-data="border-dark"]');
+const borders = document.getElementsByClassName("border--dark");
+
+window.onload = function () {
+  sliderDarkMode.checked = false;
+};
 
 sliderDarkMode.addEventListener("click", () => {
-  body.classList.toggle("darkMode__body");
-  settingsBox.classList.toggle("darkMode__card");
-  borderDark.classList.toggle("darkMode__border-dark");
+  darkMode = sliderDarkMode.checked;
 
-  console.log(borderDark);
+  if (darkMode) {
+    body.classList.add("darkMode__body");
+    settingsBox.classList.add("darkMode__card");
+    for (let i = 0; i < borders.length; i++) {
+      borders[i].style["border-color"] = "var(--color-background)";
+    }
+  } else {
+    body.classList.remove("darkMode__body");
+    settingsBox.classList.remove("darkMode__card");
+    for (let i = 0; i < borders.length; i++) {
+      borders[i].style["border-color"] = "var(--color-background-darkMode)";
+    }
+  }
+  console.log("Slider checked " + darkMode);
 });
